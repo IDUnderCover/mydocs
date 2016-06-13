3aa83f2a31bb4164135a1be657b03f665837f9ed
IDUnderCover <469792116@qq.com>
2016-06-14 00:10:18 +0800

    change port

--- flask/mywebsite/runFlask.sh
+++ flask/mywebsite/runFlask.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-app=$(docker run -d -P -v $PWD/flask:/flask  \
+app=$(docker run -d -p 80:80 -v $PWD/flask:/flask  \
 	-v $PWD/nginx.conf:/etc/nginx/nginx.conf \
 	-v $PWD/supervisord.conf:/supervisord.conf \
 	nijialiang/website -c /supervisord.conf)
