5527e016c33220e030969b2872fc34d422d462a7
IDUnderCover <469792116@qq.com>
2016-07-15 16:23:21 +0800

    add zettage pngs

--- flask/mywebsite/flask/app/view.py
+++ flask/mywebsite/flask/app/view.py
@@ -2,13 +2,10 @@
 from flask import url_for, render_template
 from app import app, flatpages
 import json
-
-
 from utils import treeview
 
 
-
-@app.route('/index/', methods = ['GET'])
+@app.route('/index/', methods=['GET'])
 @app.route('/')
 def index():
 
@@ -21,14 +18,18 @@ def page(path):
   return render_template('page.html', page=page,tree=json.dumps(treeview.get_tree_view()))
 
 
-@app.route('/blogs/', methods = ['GET'])
+@app.route('/blogs/', methods=['GET'])
 def get_blogs_list():
     return render_template('documents.html')
 
-@app.route('/webcrawlers/', methods = ['GET'])
+
+@app.route('/webcrawlers/', methods=['GET'])
 def web_crawlers():
     return render_template('webcrawlers.html')
 
+@app.route('/zettage/', methods=['GET'])
+def zettage():
+    return render_template('zettage.html')
 
 @app.errorhandler(404)
 def internal_error(error):
